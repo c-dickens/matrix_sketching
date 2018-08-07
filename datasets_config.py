25810028676600abfa7adb39004d77d37c90106d
c-dickens <c.dickens@warwick.ac.uk>
2018-08-07 11:05:27 +0100

    updated gitignore and added config for data

---
+++ datasets_config.py
@@ -0,0 +1,13 @@
+# Config file to easily extract various datasets
+
+datasets = {
+    "YearPredictionMSD" : {
+            "filepath" : "data/YearPredictionMSD.npy",
+            "repeats"  : 5,
+    },
+
+    "rail256" : {
+        "filepath" : "data/rail2586.pkl",
+        "repeats"  :  5,
+    }
+}
